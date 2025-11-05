dbfa143dfb3e0e7b487df660d611d6aaa5077fc5
brianlim05 <brianlim05@hotmail.com>
2025-11-05 11:00:18 +0800

    Added character design

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, ScrollView } from "react-native";
+import Artwork from "./components/Artwork";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ScrollView style={styles.container}>
+
+            <Text style={styles.header}>My Artwork Portfolio</Text>
+
+            <Artwork
+                title="GreenNudge Poster"
+                year="2025"
+                workType="School Work"
+                description="A sustainability-themed poster designed for GreenNudge's environmental campaign."
+                image={require("./img/GreenNudge Poster.png")}
+            />
+
+            <Artwork
+                title="PolicyPal FB Banner"
+                year="2020"
+                workType="Intern Work"
+                description="A social media banner created for PolicyPal's Facebook campaign."
+                image={require("./img/PolicyPal FB Banner.jpg")}
+            />
+
+            <Artwork
+                title="Ying Emote"
+                year="2019"
+                workType="Commission Work"
+                description="A personalised emote design created for a client's Twitch channel."
+                image={require("./img/ying emote.png")}
+            />
+
+            <Artwork
+                 title="Character Design"
+                 year="2025"
+                 workType="School Work"
+                 description="Character Design of myself in different animal outfits."
+                 image={require("./img/Character Design.png")}
+            />
+
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        padding: 15,
+    },
+    header: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#444",
+        marginBottom: 10,
+        marginTop:40,
+    },
 });
